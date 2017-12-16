bceec04b827a2b7d866a48eade27dde5ff974846
xujustinj <xu.justin.j@gmail.com>
2017-12-16 12:59:16 -0500

    boo

--- content.js
+++ content.js
@@ -1,16 +1,35 @@
 // content.js
-// var body = document.getElementsByTagName('body');
 
-divHTML = '<div id="unslack" style="';
-divHTML += 'position:fixed;'
-divHTML += 'z-index:2147483647;'
-divHTML += 'left:0px;'
-divHTML += 'top:0px;'
-divHTML += 'width:100%;'
-divHTML += 'height:100%;'
-divHTML += 'opacity:0.5;'
-divHTML += 'background:#FF8000;'
-divHTML += '"></div>';
+var style = document.createElement('style');
+style.innerHTML += '.unslackCentre {';
+style.innerHTML += 'position: absolute;';
+style.innerHTML += 'top: 50%;';
+style.innerHTML += 'left: 50%;';
+style.innerHTML += 'transform: translateX(-50%) translateY(-50%);';
+style.innerHTML += 'display: inline-block;}';
 
-document.body.innerHTML += divHTML;
-document.getElementById('unslack').innerHTML="Whatever you want it to be!";
+document.getElementsByTagName('head')[0].appendChild(style);
+
+document.body.innerHTML += '<div id="unslack"></div>';
+var unslack = document.getElementById("unslack");
+
+unslack.style.position      = "fixed";
+unslack.style.zIndex        = 2147483647;
+unslack.style.left          = "0px";
+unslack.style.top           = "0px";
+unslack.style.width         = "100%";
+unslack.style.height        = "100%";
+unslack.style.opacity       = 0.9;
+unslack.style.background    = "#000";
+
+var delay = 1000;
+
+unslack.innerHTML += '<img src="http://www.scaryforkids.com/pics/scary-pictures.jpg" class="unslackCentre" width="100%" height="100%" background-size="contain";>';
+
+setTimeout("delayedText()", delay);
+
+function delayedText() {
+    unslack.innerHTML += '<div id="delayedText">';
+    unslack.innerHTML += '<font color="orange" size=60 face="courier" class="unslackCentre"> BOO! Get back to work. </font>';
+    unslack.innerHTML += '</div>';
+}
