09277afe17636d08e1cc24f5beaa4a2fb77b0469
xujustinj <xu.justin.j@gmail.com>
2017-12-16 11:36:10 -0500

    :D
    it works now. background.js is still useless

--- content.js
+++ content.js
@@ -1,2 +1,16 @@
 // content.js
-document.body.style.backgroundColor="red";
+// var body = document.getElementsByTagName('body');
+
+divHTML = '<div id="unslack" style="';
+divHTML += 'position:fixed;'
+divHTML += 'z-index:2147483647;'
+divHTML += 'left:0px;'
+divHTML += 'top:0px;'
+divHTML += 'width:100%;'
+divHTML += 'height:100%;'
+divHTML += 'opacity:0.5;'
+divHTML += 'background:#FF8000;'
+divHTML += '"></div>';
+
+document.body.innerHTML += divHTML;
+document.getElementById('unslack').innerHTML="Whatever you want it to be!";
