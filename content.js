2731da1aa8e5d79aae8fce625e3bd0763ddd17ff
xujustinj <xu.justin.j@gmail.com>
2017-12-16 20:00:52 -0500

    Quick Fix

--- content.js
+++ content.js
@@ -14,6 +14,23 @@ var pictures = ['https://i.pinimg.com/originals/2d/0a/6b/2d0a6b931ba21705c8cd83d
 
 var picture = pictures[Math.floor(pictures.length * Math.random())];
 
+var yyyy = "" + d.getFullYear();
+var mm = twoDigits(d.getMonth() + 1);
+var dd = twoDigits(d.getDay());
+var hh = twoDigits(d.getHour());
+var mm = twoDigits(d.getMinute());
+var ss = twoDigits(d.getSecond());
+var date = yyyy + "-" + mm + "-" + dd + "_" + hh + ":" + mm + ":" + ss;
+var message = "Dear user,\n\nAt " + yyyy + "-" + mm + "-" + dd + "," + hh + ":" + mm + ":" + ss + ", an attempt was detected from your device to access a blacklisted site. We are incredibly disappointed in you, and hope you have learned from your experience.\n\nFor shame,\nThe Boo Developers";
+
+function twoDigits(num) {
+    if (num < 10) {
+        return "0" + num;
+    } else {
+        return "" + num;
+    }
+}
+
 window.onload = function() {
     s = document.createElement('style');
     s.innerHTML += '.booCentreMe {';
@@ -38,23 +55,6 @@ window.onload = function() {
 
     setTimeout("pop()", delayMin + Math.floor((delayMax - delayMin) * Math.random()));
 };
-
-var yyyy = "" + d.getFullYear();
-var mm = twoDigits(d.getMonth() + 1);
-var dd = twoDigits(d.getDay());
-var hh = twoDigits(d.getHour());
-var mm = twoDigits(d.getMinute());
-var ss = twoDigits(d.getSecond());
-var date = yyyy + "-" + mm + "-" + dd + "_" + hh + ":" + mm + ":" + ss;
-var message = "Dear user,\n\nAt " + yyyy + "-" + mm + "-" + dd + "," + hh + ":" + mm + ":" + ss + ", an attempt was detected from your device to access a blacklisted site. We are incredibly disappointed in you, and hope you have learned from your experience.\n\nFor shame,\nThe Boo Developers";
-
-function twoDigits(num) {
-    if (num < 10) {
-        return "0" + num;
-    } else {
-        return "" + num;
-    }
-}
 
 function pop() {
     boo.style.zIndex = 2147483647;
