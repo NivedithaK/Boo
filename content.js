3293474f53c67412241814c96e5efb133bf678b9
xujustinj <xu.justin.j@gmail.com>
2018-01-12 19:29:17 -0500

    I don't know what this update is

--- content.js
+++ content.js
@@ -8,9 +8,7 @@ var delayMax = 10000;
 
 var pictures = ['https://i.pinimg.com/originals/2d/0a/6b/2d0a6b931ba21705c8cd83df8e545f21.gif',
                 'http://www.scaryforkids.com/pics/scary-pictures.jpg',
-                'https://i.ytimg.com/vi/Z8l7yU_eZd8/maxresdefault.jpg',
-                'http://assets1.ignimgs.com/2017/08/16/it-11951r-1502893105886_1280w.jpg',
-                'https://i.redd.it/2roptu7kwxhy.gif'];
+                'https://i.ytimg.com/vi/Z8l7yU_eZd8/maxresdefault.jpg'];
 
 var picture = pictures[Math.floor(pictures.length * Math.random())];
 
@@ -73,10 +71,7 @@ function pop() {
     boo.style.opacity = 1;
     boo.innerHTML += '<img src="' + picture + '" class="booCentreMe" style="width: 100%; height: auto;"">';
     boo.innerHTML += '<div class="booCentreMe" style="font: 192px helvetica, sans-serif; color: white; opacity: 0.75; text-align: center;">BOO!</div>';
-    boo.innerHTML += '<div class="booCentreMe" style="font: 24px helvetica, sans-serif; color: white; opacity: 0.75; text-align: center;"><br><br><br><br><br><br><br>Get back to work. Click to close.</div>';
-    boo.addEventListener('click', function(event) {
-        close();
-    });
+    boo.innerHTML += '<div class="booCentreMe" style="font: 24px helvetica, sans-serif; color: white; opacity: 0.75; text-align: center;"><br><br><br><br><br><br><br>Get back to work.</div>';
 
     download("procrastination-log_" + date + ".txt", message);
 }
